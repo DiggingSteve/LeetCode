9b74e9c018da8c23bc80bfa28941b4239655e6fa
Steve <doublefly@live.cn>
2022-08-22 17:37:49 +0800

    []

--- LeetCode/LeetCode/Program.cs
+++ LeetCode/LeetCode/Program.cs
@@ -2,19 +2,37 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
-using LeetCode.Problem;
 
-namespace LeetCode
+namespace GuidCollisionDetector
 {
     class Program
     {
+        public static int bitSquareSum(int n)
+        {
+            int sum = 0;
+            while (n > 0)
+            {
+                int bit = n % 10;
+                sum += bit * bit;
+                n = n / 10;
+            }
+            return sum;
+        }
+
+        public static bool isHappy(int n)
+        {
+            int slow = n, fast = bitSquareSum(n);
+            while (slow != fast)
+            {
+                slow = bitSquareSum(slow);
+                fast = bitSquareSum(fast);
+                fast = bitSquareSum(fast);
+            }
+            return slow == 1;
+        }
         static void Main(string[] args)
         {
-            var a = new ProAddTwoNum();
-            var l1 = new ProAddTwoNum.ListNode(1) { next = new ProAddTwoNum.ListNode(8) { next = new ProAddTwoNum.ListNode(3) } };
-            var l2 = new ProAddTwoNum.ListNode(0);
-            a.AddTwoNumbers(l1, l2);
+            isHappy(19);
         }
     }
 }
---
+++ leet/src/main/java/Dfs.java
@@ -0,0 +1,9 @@
+/**
+ * @description:
+ * @author: yaodui
+ * @time: 2022/8/22 17:37
+ */
+
+public class Dfs {
+
+}
